ca609ab2d5ad53bc6087870b5788a6bed89212e5
SedDiaz69 <86267624+SedDiaz69@users.noreply.github.com>
2021-07-14 16:16:38 +0200

    Update Main.java
    ok

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
-mkdir –p /tmp/ajc
-rm –rf /tmp/ajc/*
-echo '
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World !") ;
+    System.out.println("Hello, Sed !") ;
   }
 }
-' > /tmp/ajc/Main.java
-javac /tmp/ajc/Main.java
